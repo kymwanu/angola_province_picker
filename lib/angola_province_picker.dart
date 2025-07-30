1945d82e465ed89a806279bf5a5735ac328890b5
Kimuano Luís <kimuano.luis@ksolid.ao>
2025-07-30 08:46:08 +0100

    Including an example and documenting the package

--- lib/angola_province_picker.dart
+++ lib/angola_province_picker.dart
@@ -6,10 +6,10 @@ library;
 
 export 'src/models/towns.dart';
 export 'src/models/provinces.dart';
-export 'src/widgets/angola_province_picker.dart';
 export 'src/utils/angola_picker_mode.dart';
-export 'src/widgets/cupertino/cupertino_province_picker.dart';
-export 'src/widgets/material/material_province_picker.dart';
 export 'src/widgets/angola_town_picker.dart';
+export 'src/widgets/angola_province_picker.dart';
+export 'src/widgets/cupertino/cupertino_province_picker.dart';
 export 'src/widgets/cupertino/cupertino_town_picker.dart';
+export 'src/widgets/material/material_province_picker.dart';
 export 'src/widgets/material/material_town_picker.dart';
